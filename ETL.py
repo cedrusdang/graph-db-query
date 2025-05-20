a975ed4de4f2a6c06f890fd869d73b3177188210
cedrusdang <dtt161094@gmail.com>
2025-05-21 00:49:49 +0800

    Update the schema, add functions in ETL

---
+++ ETL.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import pandas as pd
+
+def extract(file_name: str, drop_na: bool = True) -> pd.DataFrame:
+    # Extract the root path
+    root_path = Path(__file__).resolve().parent
+    # Asembling the path
+    csv_path = root_path / "data" / file_name
+
+    # Extract the main data csv file
+    data = pd.read_csv(csv_path)
+
+    # Drop rows that contain na if needed
+    if drop_na:
+        data.dropna()
+    
+    # Ensure all cols is string (Before further transformation)
+    data = data.astype(str)
+
+    return(data)
+
+data = extract("ARDD_Fatalities_Dec_2024.csv")
